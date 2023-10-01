a8fa1042e62164fa3171d3d3c8e92e7b0489525b
Harimohan <harimohan.h@prod.hclpnp.com>
2023-10-01 21:29:12 +0530

    added modal for editing note and added form in modal

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,20 +1,69 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useRef, useState} from "react";
 import NoteContext from "../context/notes/noteContext";
 import Noteitem from "./Noteitem";
-import AddNote from "./AddNote";
 function Note() {
     const context= useContext(NoteContext)
-    const {notes,getAllNotes} = context
+    const {notes,getAllNotes,editNote} = context
     useEffect(()=>{
         getAllNotes()
-    })
+    }, [])
+    const ref = useRef(null)
+    const refClose= useRef()
+    const [note, setNote] = useState({id:"",etitle: "", edescription: "", etag: "default"})
+    const updateNote = (currentNote)=>{
+        ref.current.click()
+        setNote({id:currentNote._id, etitle:currentNote.title, edescription:currentNote.description,etag:currentNote.tag})
+    }
+    const handleClick = (e)=>{
+        editNote(note.id,note.etitle,note.edescription,note.etag)
+        refClose.current.click()
+    }
+    const onChange = (e)=>{
+        setNote({...note, [e.target.name]: e.target.value})    // it means whatever change will be target that will be update here
+    }
     return (
-        <div className="row my-3">
-            <h2>You Notes</h2> 
-            {notes.map((note)=>{
-                return <Noteitem key={note._id} note={note}/>  
-            })}
+        <>
+            <button type="button" className="btn btn-primary d-none" ref={ref}data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Launch demo modal
+            </button>
+
+            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div className="modal-dialog">
+                <div className="modal-content">
+                <div className="modal-header">
+                    <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div className="modal-body">
+                    <form className="my-3">
+                    <div className="mb-3">
+                        <label htmlFor="title" className="form-label">Title</label>
+                        <input type="text" className="form-control" id="etitle" name="etitle" value={note.etitle} aria-describedby="emailHelp" onChange={onChange} /> 
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="description" className="form-label">Description</label>
+                        <input type="text" className="form-control" id="edescription" name="edescription" value={note.edescription} onChange={onChange} />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="tag" className="form-label">Tag</label>
+                        <input type="text" className="form-control" id="etag" name="etag" value={note.etag} onChange={onChange} />
+                    </div>
+                    </form>
+                </div>
+                <div className="modal-footer">
+                    <button type="button" ref={refClose} className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" className="btn btn-primary" onClick={handleClick}>update Note</button>
+                </div>
+                </div>
             </div>
+            </div>
+            <div className="row my-3">
+                <h2>You Notes</h2> 
+                {notes.map((note)=>{
+                    return <Noteitem key={note._id} updateNote={updateNote} note={note}/>  
+                })}
+            </div>
+        </>
     )
 }
 
--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -1,7 +1,8 @@
 import React,{useContext} from "react";
 import NoteContext from "../context/notes/noteContext";
 function Noteitem(props) {
-  const note = props.note;
+  const {note,updateNote} = props;
+  console.log(props)
   const context=useContext(NoteContext)
   const {deleteNote} = context
   const handleDelete= ()=>{
@@ -13,7 +14,7 @@ function Noteitem(props) {
                 <div className="card-body">
                     <div className="d-flex align-items-center">
                         <h5 className="card-title">{note.title}</h5><i className= "far fa-trash-alt mx-2 " onClick={handleDelete}></i>
-                        <i className="far fa-edit mx-2"></i>
+                        <i className="far fa-edit mx-2" onClick={()=>{updateNote(note)}}></i>
                     </div>
                     <p className="card-text">{note.description}</p>
 
